5c7fe4c069fc0806191ef0201bffd438f938e164
Daniel Westerlund <westerlund.da@gmail.com>
2024-10-12 12:52:36 +0200

    Refinements responsive

--- frontend/recipify/src/components/search/refinements/filters/rangeFilters.tsx
+++ frontend/recipify/src/components/search/refinements/filters/rangeFilters.tsx
@@ -7,7 +7,7 @@ const ranges = [
 
 export default function RangeFilters() {
   return (
-    <div className="mt-4 text-xs tracking-wider text-white">
+    <div className="tracking-wider text-white">
       <h3 className="font-semibold">Published range</h3>
       {ranges.map((o) => (
         <RangeFilter key={o.param} param={o.param} label={o.label} />
@@ -20,7 +20,7 @@ const RangeFilter = ({ param, label }: { param: string; label: string }) => {
   const { filters, setFilterParameter, removeFilter } = useSearchContext();
 
   return (
-    <div className="mt-2 flex">
+    <div className="mt-2 flex text-blue">
       <label htmlFor={param}>{label}:</label>
       <input
         type="date"
@@ -34,7 +34,7 @@ const RangeFilter = ({ param, label }: { param: string; label: string }) => {
         value={filters.find((f) => f.key === param)?.value || ""}
         id={param}
         name={param}
-        className="flex w-full bg-darkBlue px-2 text-white/90 outline-none"
+        className="flex w-full bg-darkBlue px-2 outline-none"
       />
     </div>
   );
--- frontend/recipify/src/components/search/refinements/refinements.tsx
+++ frontend/recipify/src/components/search/refinements/refinements.tsx
@@ -2,6 +2,7 @@ import { AnimatePresence, motion } from "framer-motion";
 import { useSearchContext } from "../../../contexts/useSearchContext";
 import { useState } from "react";
 import RangeFilters from "./filters/rangeFilters";
+import { RiCloseLargeLine } from "react-icons/ri";
 
 export default function Refinements() {
   const { results } = useSearchContext();
@@ -11,11 +12,13 @@ export default function Refinements() {
 
   return (
     <div className="w-full">
-      <div className="mt-4 flex w-full max-w-xs justify-evenly">
-        {results && (
+      <div className="mx-auto mt-4 flex w-full max-w-xs justify-between">
+        {results ? (
           <p className="py-1 text-xs font-semibold tracking-wider text-white/90">
             {results.total.toLocaleString()} hits
           </p>
+        ) : (
+          <p></p>
         )}
         <motion.button
           whileTap={{ scale: 0.95 }}
@@ -53,10 +56,17 @@ const FilterModal = ({
           onClick={close}
           className={`fixed inset-0 z-50 flex justify-center bg-black/50`}
         >
-          <motion.div className="mt-[50%] h-fit w-full max-w-xs rounded-md bg-darkBlue p-4 shadow-black-lg">
-            <h2 className="text-center font-semibold tracking-wider text-aqua">
+          <motion.div className="relative mt-52 h-fit w-full max-w-xs rounded-md bg-darkBlue p-4 text-xs shadow-black-lg md:max-w-md md:px-8 md:pb-8 md:text-sm xl:max-w-2xl xl:text-lg">
+            <h2 className="mb-4 text-center text-lg font-semibold tracking-wider text-blue md:mb-8 md:text-2xl xl:mb-12 xl:text-4xl">
               Filter Results
             </h2>
+            <button
+              type="button"
+              onClick={handleOpen}
+              className="absolute inset-2 h-fit w-fit text-xl text-blue xl:inset-4 xl:text-3xl"
+            >
+              <RiCloseLargeLine />
+            </button>
             <RangeFilters />
           </motion.div>
         </motion.div>
